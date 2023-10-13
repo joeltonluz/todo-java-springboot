3e154ccf40adf3110a2eb2dc27dfc1fba31274c3
joeltonluz <joelton.luz@bughunt.com.br>
2023-10-13 17:45:15 -0300

    feat: include healthy controller to test

---
+++ src/main/java/github/com/joeltonluz/todolist/health/HealthController.java
@@ -0,0 +1,15 @@
+package github.com.joeltonluz.todolist.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthController {
+  @GetMapping("")
+  public String healthy() {
+    return "Healthy";
+  }
+
+}
